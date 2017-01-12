46a38cf53becb0fcc6cbfad5a6ccdd3515c9531d
Serena Chan <serena.sy.c@gmail.com>
2017-01-11 16:11:43 -0800

    ?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = "wss://sitepoint-actioncable.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://serenasyc-chatterbox.herokuapp.com/',
-                                               'http://serenasyc-chatterbox.herokuapp.com/']
+  config.action_cable.allowed_request_origins = ['https://ssyc-chatbox.herokuapp.com/',
+                                               'http://ssyc-chatbox.herokuapp.com/']
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
